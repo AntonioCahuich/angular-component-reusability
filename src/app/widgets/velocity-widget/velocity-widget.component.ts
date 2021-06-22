d1568db32df293b0ff0808d414cae9bd8c88766a
Antonio Cahuich Huchin <antonio.cahuich.huchin@gmail.com>
2021-06-22 16:46:04 -0500

    bridge pattern / widgets components

---
+++ src/app/widgets/velocity-widget/velocity-widget.component.ts
@@ -0,0 +1,30 @@
+import {Component} from '@angular/core';
+import {Widget} from "../widget.interface";
+import {WIDGET} from "../widgte.token";
+
+@Component({
+  selector: 'app-velocity-widget',
+  templateUrl: './velocity-widget.component.html',
+  styleUrls: ['./velocity-widget.component.scss'],
+  providers: [
+    {
+      provide: WIDGET,
+      useExisting: VelocityWidgetComponent,
+    }
+  ]
+})
+export class VelocityWidgetComponent implements Widget {
+  isRefreshing = false;
+
+  load() {
+    console.log("Load data from JIRA API...")
+  }
+
+  refresh() {
+    this.isRefreshing = true;
+
+    setTimeout(() => {
+      this.isRefreshing = false;
+    }, 2500);
+  }
+}
---
+++ src/app/widgets/weather-widget/weather-widget.component.ts
@@ -0,0 +1,30 @@
+import {Component} from '@angular/core';
+import {Widget} from "../widget.interface";
+import {WIDGET} from "../widgte.token";
+
+@Component({
+  selector: 'app-weather-widget',
+  templateUrl: './weather-widget.component.html',
+  styleUrls: ['./weather-widget.component.scss'],
+  providers: [
+    {
+      provide: WIDGET,
+      useExisting: WeatherWidgetComponent
+    }
+  ]
+})
+export class WeatherWidgetComponent implements Widget {
+  isLoading = false;
+
+  load() {
+    console.log("Load data from WEATHER API...")
+  }
+
+  refresh() {
+    this.isLoading = true;
+
+    setTimeout(() => {
+      this.isLoading = false;
+    }, 1000)
+  }
+}
